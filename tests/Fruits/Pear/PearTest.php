7a146e5a88de5e5737c1717106a3f825379b8f83
ha_tora <j.2374.04@gmail.com>
2023-02-17 20:09:39 +0600

    Tests

---
+++ tests/Fruits/Pear/PearTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Test\Orchard\Fruits\Pear;
+
+class PearTest extends TestCase
+{
+    protected $pear;
+
+    public function setUp(): void
+    {
+        $this->pear = new Pear;
+    }
+
+    public function test_pear_is_pear(): void
+    {
+        $this->assertEquals($this->pear->getFruitType(), 'Pear');
+    }
+
+    public function test_pear_weight(): void
+    {
+        $weight = $this->pear->getWeight();
+
+        $this->assertTrue($weight >= 130);
+        $this->assertTrue($weight <= 170);
+    }
+}
